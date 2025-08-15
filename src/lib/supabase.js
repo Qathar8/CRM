de779d4d4c7397e01355701914a1f5bc54be1cda
Qathar8 <qureshqathar10@gmail.com>
2025-08-15 10:27:53 +0300

    Remove corrupted JS files causing syntax errors

--- src/lib/supabase.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-{
-    "code";
-    "rate-limited", "message";
-    "You have hit the rate limit. Please upgrade to keep chatting.", "providerLimitHit";
-    false, "isRetryable";
-    true;
-}
